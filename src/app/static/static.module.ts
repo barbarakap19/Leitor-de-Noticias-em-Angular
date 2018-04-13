539c3eb52aadcfb262903390a02e592fa2eefb88
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-13 15:32:22 -0300

    criado componet contato

--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StaticRoutingModule } from './static-routing.module';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   imports: [
     CommonModule,
     StaticRoutingModule
   ],
-  declarations: []
+  declarations: [ContatoComponent]
 })
 export class StaticModule { }
