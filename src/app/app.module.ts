939bd373e24f54c401b610a0ea3d2dba5e84d1fe
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-13 14:42:12 -0300

    criado o componente feed e router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
